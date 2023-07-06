a431243860f10ae29f533dcfa0c2ed2fd27b2727
Alex Samtsevich <61207985+hanktet@users.noreply.github.com>
2023-07-06 14:24:16 +0300

    add трение

--- game/js/GameScene.js
+++ game/js/GameScene.js
@@ -1,53 +1,56 @@
 class GameScene extends Phaser.Scene {
-  constructor() {
-    super("GameScene");
-  }
-  preload() {
-    this.load.image("bg", "assets/background.jpeg");
-    this.load.image("ball", "assets/ball.png");
-  }
-  create() {
-    this.createBackgraund();
-    this.createObjects();
-  }
-  createBackgraund() {
-    this.add.image(0, 0, "bg").setOrigin(0);
-  }
+    constructor() {
+        super("GameScene");
+    }
+    preload() {
+        this.load.image("bg", "assets/background.jpeg");
+        this.load.image("ball", "assets/ball.png");
+    }
+    create() {
+        this.createBackgraund();
+        this.createObjects();
+    }
+    createBackgraund() {
+        this.add.image(0, 0, "bg").setOrigin(0);
+    }
 
-  startMove() {
-    this.firstBall.setVelocity(200, 200).setDrag(0.2);
-    this.startGame = true;
-  }
+    startMove() {
+        this.firstBall.setVelocity(2000, 2000).setDrag(0.2);
+        this.startGame = true;
+    }
 
-  restartMove() {
-    this.firstBall
-      .setVelocity(0)
-      .setPosition(config.width / 2, config.height / 2);
-    this.startGame = false;
-  }
+    restartMove() {
+        this.firstBall
+            .setVelocity(0)
+            .setPosition(config.width / 2, config.height / 2);
+        this.startGame = false;
+    }
 
-  createObjects() {
-    this.firstBall = this.physics.add
-      .image(config.width / 2, config.height / 2, "ball")
-      .setVelocity(0, 0)
-      .setBounce(1, 1)
-      .setCollideWorldBounds(true)
-      .setFriction(1)
-      .setDrag(0.2) // Значение трения, можно изменять для разных эффектов
-      .setMaxVelocity(400); // Максимальная скорость мяча
+    createObjects() {
+        this.firstBall = this.physics.add
+            .image(config.width / 2, config.height / 2, "ball")
+            .setVelocity(0, 0)
+            .setBounce(1, 1)
+            .setCollideWorldBounds(true);
 
-    this.physics.world.enable([this.firstBall]);
+        this.firstBall.setAcceleration(0, 0);
 
-    this.input.on("pointerdown", (pointer) => {
-      if (!this.startGame) {
-        this.startMove();
-      } else {
-        this.restartMove();
-      }
-    });
-  }
+        this.physics.world.enable([this.firstBall]);
 
-  update() {
-    this.firstBall.rotation = this.firstBall.body.angle;
-  }
+        this.input.on("pointerdown", (pointer) => {
+            if (!this.startGame) {
+                this.startMove();
+            } else {
+                this.restartMove();
+            }
+        });
+    }
+
+    update() {
+        if (this.startGame) {
+            // Замедление мяча
+            const deceleration = 2; // Величина замедления, можно изменять
+            this.firstBall.setAcceleration(-this.firstBall.body.velocity.x * deceleration, -this.firstBall.body.velocity.y * deceleration);
+        }
+    }
 }
